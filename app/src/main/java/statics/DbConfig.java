911bbf84e97bdd2575493b9d558c15be01aaac3a
Vikramjeet Singh <70337807+vsingh2201@users.noreply.github.com>
2024-03-31 14:06:50 -0400

    Update DBconfig

--- app/src/main/java/statics/DbConfig.java
+++ app/src/main/java/statics/DbConfig.java
@@ -2,8 +2,8 @@ package statics;
 
 public class DbConfig {
     public static final String DB_CONNECTION_STRING = "jdbc:postgresql://localhost:5432/farmerhub";
-    public static final String DB_USER = "vikramjeetsingh";
-    public static final String DB_PASSWORD = "Vikram@21";
+    public static final String DB_USER = "user";
+    public static final String DB_PASSWORD = "password";
 
-    public static final boolean IS_MOCK = false;
+    public static final boolean IS_MOCK = true;
 }
