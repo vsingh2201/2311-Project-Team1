16c5060b69d5eecb5f8a979caa7a1fef7fb287d1
Vikramjeet Singh <70337807+vsingh2201@users.noreply.github.com>
2024-03-31 14:01:31 -0400

    Add Integration Tests for ItemRepository

--- app/src/intTest/java/integ/IntegrationTest.java
+++ app/src/intTest/java/integ/IntegrationTest.java
@@ -4,21 +4,32 @@ import utils.DatabaseSeedingUtil;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import models.Item;
 import models.User;
+import repositories.itemRepository.ItemRepository;
 import repositories.userRepository.UserRepository;
+
 
 public class IntegrationTest {
     private static UserRepository userRepository;
+    private static ItemRepository itemRepository;
     
     @BeforeAll
     public static void setUp() {
+    	// Set up database
         DatabaseSeedingUtil db = new DatabaseSeedingUtil();
         db.seedDatabase();
         userRepository = new UserRepository();
+        itemRepository = new ItemRepository();
     }
+    
+    // Test Cases for UserRepository
     
     @Test
     public void testGetUserById() {
@@ -53,4 +64,43 @@ public class IntegrationTest {
         User invalidUser = userRepository.validateUser("jimbeam", "wrongpassword");
         assertNull(invalidUser);
     }
+    
+    // Test cases for ItemRepository
+    @Test
+	public void testGetAllItems() {
+		List<Item> allItems = itemRepository.getAllItems();
+		assertEquals(4, allItems.size());
+	}
+    
+    @Test
+	public void testGetItemById() {
+		Item item = itemRepository.getItemById(2);
+		assertNotNull(item);
+		assertEquals("Harvester", item.getName());
+	}
+    
+    @Test
+	public void testGetItemByName() {
+		Item item = itemRepository.getItemByName("Corn");
+		assertNotNull(item);
+		assertEquals("Corn", item.getName());
+	}
+    @Test
+	public void testGetItemsByIds() {
+		List<Integer> itemIds = new ArrayList<>();
+		itemIds.add(1);
+		itemIds.add(2);
+		List<Item> items = itemRepository.getItemsByIds(itemIds);
+		assertEquals(2, items.size());
+	}
+
+	@Test
+	public void testGetItemByFarmerIdAndName() {
+		Item item = itemRepository.getItemByFarmerIdAndName(1, "Corn");
+		assertNotNull(item);
+		assertEquals("Corn", item.getName());
+		assertEquals(1, item.getFarmerId());
+	}
+
+    
 }
